e02e99cf462e1662cf5b694d7c21fb9553063e76
Arthur Sottoriva <arthursottoriva49@gmail.com>
2025-04-14 12:04:00 -0300

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import "./styles/theme.css";
 import "./styles/global.css";
 import { Heading } from "./components/Heading";
+import { TimerIcon } from "lucide-react";
+import { Container } from "./components/Container";
 
 //PascalCase
 
 export function App() {
-  console.log("Oi");
-
   return (
     <>
-      <Heading attr={123} attr2="String">
-        Bankai
-      </Heading>
-      <Heading>Bankai 2</Heading>
-      <h1>Ola mundo (do App)</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ullam
-        sunt aspernatur dicta laborum reiciendis quia tenetur aut,
-        necessitatibus deleniti, placeat eveniet ipsam dolore culpa ut
-        accusamus. Ipsum, eius incidunt.
-      </p>
+      <Container>
+        <Heading>TESTANDO COMPONENTE HEADING</Heading>
+      </Container>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,5 @@ import { App } from "./App";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-    <App />
-    <h1>blabla</h1>
   </StrictMode>
 );

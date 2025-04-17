b9868c055df207559637a75f28c29a81b9777ead
Arthur Sottoriva <arthursottoriva49@gmail.com>
2025-04-17 16:28:48 -0300

    aa!

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import "./styles/theme.css";
 import "./styles/global.css";
 import { NotFound } from "./pages/NotFound";
-import { Home } from "./pages/Home";
 
 export function App() {
-  return <Home />;
+  return <NotFound />;
 }

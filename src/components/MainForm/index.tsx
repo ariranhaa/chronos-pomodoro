ecf69be46774bb89af38c0e869971c7c9b50e835
Arthur Sottoriva <arthursottoriva49@gmail.com>
2025-04-28 12:12:09 -0300

    att

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,41 +1,110 @@
-import { PlayCircleIcon } from "lucide-react";
+import { PlayCircleIcon, StopCircleIcon } from "lucide-react";
 import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { Input } from "../Input";
-import { useTaskContext } from "../../contexts/TaskContext";
+import { useRef } from "react";
+import { TaskModel } from "../../models/TaskModel";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { getNextCycle } from "../../utils/getNestCycle";
+import { getNextCycleType } from "../../utils/getNextCycleType";
+import { formatSecondsToMinutes } from "../../utils/formatSecodsToMinutes";
 
 export function MainForm() {
-  const { setState } = useTaskContext();
-  function handleClick() {
+  const { state, setState } = useTaskContext();
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
+  const nextCycle = getNextCycle(state.currentCycle);
+  const nextCycleType = getNextCycleType(nextCycle);
+
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert("Digitie o nome da tarefa");
+      return;
+    }
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: state.config[nextCycleType],
+      type: nextCycleType,
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
     setState((prevState) => {
-      return { ...prevState, formattedSecondsRemaining: "21:00" };
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: nextCycle,
+        secondsRemaining,
+        formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining),
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
+  }
+
+  function handleInterruptTask() {
+    setState((prevState) => {
+      return {
+        ...prevState,
+        activeTask: null,
+        secondsRemaining: 0,
+        formattedSecondsRemaining: "00:00",
+      };
     });
   }
 
   return (
-    <form className="form" action="">
-      <button onClick={handleClick} type="button">
-        clicar
-      </button>
+    <form onSubmit={handleCreateNewTask} className="form" action="">
       <div className="formRow">
         <Input
           labelText="task"
           id="inputTask"
           type="text"
           placeholder="Digite algo"
+          ref={taskNameInput}
+          disabled={!!state.activeTask}
         />
       </div>
 
       <div className="formRow">
         <p>Proximo intervalo é de 25min</p>
       </div>
+      {state.currentCycle > 0 && (
+        <div className="formRow">
+          <Cycles />
+        </div>
+      )}
 
       <div className="formRow">
-        <Cycles />
-      </div>
-
-      <div className="formRow">
-        <DefaultButton icon={<PlayCircleIcon />} />
+        {!state.activeTask && (
+          <DefaultButton
+            aria-label="Iniciar nova tarefa"
+            title="Iniciar nova tarefa"
+            type="submit"
+            icon={<PlayCircleIcon />}
+            key="Botão de submit"
+          />
+        )}{" "}
+        {!!state.activeTask && (
+          <DefaultButton
+            aria-label="Interromper tarefa"
+            title="Interromper tarefa"
+            type="button"
+            color="red"
+            icon={<StopCircleIcon />}
+            onClick={handleInterruptTask}
+          />
+        )}
       </div>
     </form>
   );
